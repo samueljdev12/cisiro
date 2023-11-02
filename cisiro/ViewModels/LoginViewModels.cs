e9163a4a8d9b85ec48c36b5fd0ebd82bba7ce23c
D-1963 <desilvadeepthi@yahoo.com.au>
2023-11-02 14:54:39 +1100

    dt

---
+++ cisiro/ViewModels/LoginViewModels.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace cisiro.ViewModels
+{
+    public class LoginViewModels
+    {
+        public string Email { get; set; }   
+
+        public string Password { get; set; }    
+
+        public bool RememberMe { get; set; }
+    }
+}
