e6f02f76c9b08cb7e40009ca63b71fb04574cdf0
samueljdev12 <109645212+samueljdev12@users.noreply.github.com>
2023-11-02 15:03:02 +1100

    register view model

---
+++ cisiro/Controllers/ApplicationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cisiro.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Apply()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ cisiro/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cisiro.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "First name is required")]
+        public string firstName { get; set; }
+        [Required(ErrorMessage = "last name is required")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Qualification is required")]
+        public string qualification { get; set; }
+
+        [Required(ErrorMessage = "email is required")]
+        [EmailAddress]
+        public string email { get; set; }
+        [Required(ErrorMessage = "mobile number is required")]
+        public string mobileNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("password", ErrorMessage ="Passwords do not match")]
+        public string confirmPassword { get; set; }
+    }
+}
