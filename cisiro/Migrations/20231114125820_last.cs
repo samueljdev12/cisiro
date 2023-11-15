3356ae561c77d78c5f59c1919aa049f7633e2664
Jonah Samuel <samueljonas922@gmail.com>
2023-11-16 08:15:42 +1100

    Comiit all

---
+++ cisiro/Migrations/20231114125820_last.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cisiro.Migrations
+{
+    public partial class last : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "coverLetter",
+                table: "application",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "coverLetter",
+                table: "application");
+        }
+    }
+}
